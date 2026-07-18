9362c740f6924379827e640a9f914192f8324966
Derek Pitman <derek.j.pitman@gmail.com>
2026-07-18 15:54:25 -0400

    Rewrite: IDs need to be globally unique

--- main.go
+++ main.go
@@ -19,19 +19,14 @@ import (
 //   MSGRECV <ns> <qid>  -> dequeue head; print msg or EMPTY
 //   PEEK <ns_a> <ns_b> <key>  -> 1 if visible in ns_b, else 0 (0 if ns_a != ns_b)
 
-type MsgQueue struct {
-	Key      string
-	Messages []string
-}
-
-type SHM struct {
-	Key  string
-	Size int
+type IdKey struct {
+	Namespace int
+	Key       string
 }
 
 type IPC struct {
-	Mem    []SHM
-	Queues []MsgQueue
+	Mem    map[string]int
+	Queues map[string][]string
 }
 
 func main() {
@@ -40,6 +35,8 @@ func main() {
 	var out []string
 	// TODO: declare your state structures here
 	var namespaces []IPC
+	var memIds []IdKey
+	var queueIds []IdKey
 	for sc.Scan() {
 		line := strings.TrimSpace(sc.Text())
 		if line == "" {
@@ -49,62 +46,54 @@ func main() {
 		switch parts[0] {
 		case "NEWNS":
 			// TODO: new ns; print id
-			namespaces = append(namespaces, IPC{[]SHM{}, []MsgQueue{}})
+			namespaces = append(namespaces, IPC{map[string]int{}, map[string][]string{}})
 			out = append(out, strconv.Itoa(len(namespaces)))
 		case "SHMGET":
 			// TODO: OK <id> or EEXIST
 			nsId, _ := strconv.Atoi(parts[1])
-			found := false
-			for _, k := range namespaces[nsId-1].Mem {
-				if k.Key == parts[2] {
-					found = true
-					break
-				}
-			}
-			if found {
+			if _, exists := namespaces[nsId-1].Mem[parts[2]]; exists {
 				out = append(out, "EEXIST")
 			} else {
 				size, _ := strconv.Atoi(parts[3])
-				namespaces[nsId-1].Mem = append(namespaces[nsId-1].Mem, SHM{parts[2], size})
-				out = append(out, fmt.Sprintf("OK %d", len(namespaces[nsId-1].Mem)))
+				namespaces[nsId-1].Mem[parts[2]] = size
+				memIds = append(memIds, IdKey{nsId - 1, parts[2]})
+				out = append(out, fmt.Sprintf("OK %d", len(memIds)))
 			}
 		case "SHMLIST":
 			// TODO: '<id> <key> <size>' sorted by id
 			nsId, _ := strconv.Atoi(parts[1])
 			var list []string
-			for i, k := range namespaces[nsId-1].Mem {
-				list = append(list, fmt.Sprintf("%d %s %d", i+1, k.Key, k.Size))
+			for i, k := range memIds {
+				if k.Namespace == nsId-1 {
+					list = append(list, fmt.Sprintf("%d %s %d", i+1, k.Key, namespaces[nsId-1].Mem[k.Key]))
+				}
 			}
 			out = append(out, strings.Join(list, "\n"))
 		case "MSGGET":
 			// TODO: OK <id> or EEXIST
 			nsId, _ := strconv.Atoi(parts[1])
-			found := false
-			for _, k := range namespaces[nsId-1].Queues {
-				if k.Key == parts[2] {
-					found = true
-					break
-				}
-			}
-			if found {
+			if _, exists := namespaces[nsId-1].Queues[parts[2]]; exists {
 				out = append(out, "EEXIST")
 			} else {
-				namespaces[nsId-1].Queues = append(namespaces[nsId-1].Queues, MsgQueue{parts[2], []string{}})
-				out = append(out, fmt.Sprintf("OK %d", len(namespaces[nsId-1].Queues)))
+				namespaces[nsId-1].Queues[parts[2]] = []string{}
+				queueIds = append(queueIds, IdKey{nsId - 1, parts[2]})
+				out = append(out, fmt.Sprintf("OK %d", len(queueIds)))
 			}
 		case "MSGSEND":
 			// TODO: enqueue; OK
 			nsId, _ := strconv.Atoi(parts[1])
 			qId, _ := strconv.Atoi(parts[2])
-			namespaces[nsId-1].Queues[qId].Messages = append(namespaces[nsId-1].Queues[qId].Messages, parts[3])
+			queueKey := queueIds[qId-1].Key
+			namespaces[nsId-1].Queues[queueKey] = append(namespaces[nsId-1].Queues[queueKey], parts[3])
 			out = append(out, "OK")
 		case "MSGRECV":
 			// TODO: dequeue head; print msg or EMPTY
 			nsId, _ := strconv.Atoi(parts[1])
 			qId, _ := strconv.Atoi(parts[2])
-			if len(namespaces[nsId-1].Queues[qId].Messages) > 0 {
-				out = append(out, namespaces[nsId-1].Queues[qId].Messages[0])
-				namespaces[nsId-1].Queues[qId].Messages = namespaces[nsId-1].Queues[qId].Messages[1:]
+			queueKey := queueIds[qId-1].Key
+			if len(namespaces[nsId-1].Queues[queueKey]) > 0 {
+				out = append(out, namespaces[nsId-1].Queues[queueKey][0])
+				namespaces[nsId-1].Queues[queueKey] = namespaces[nsId-1].Queues[queueKey][1:]
 			} else {
 				out = append(out, "EMPTY")
 			}
@@ -115,14 +104,7 @@ func main() {
 			if ns1 != ns2 {
 				out = append(out, "0")
 			} else {
-				found := false
-				for _, k := range namespaces[ns1-1].Mem {
-					if k.Key == parts[3] {
-						found = true
-						break
-					}
-				}
-				if found {
+				if _, found := namespaces[ns1-1].Mem[parts[3]]; found {
 					out = append(out, "1")
 				} else {
 					out = append(out, "0")
