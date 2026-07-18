d88b9b43d866fef66b84396d30f4e2b3cfdd0ca1
Derek Pitman <derek.j.pitman@gmail.com>
2026-07-17 23:24:25 -0400

    Lesson 05

--- main.go
+++ main.go
@@ -8,28 +8,26 @@ import (
 	"strings"
 )
 
-// User Namespace Translator
-// Bidirectional uid_map: in-ns id <-> host id, via ranges (in, host, length).
+// UTS Namespace Isolation
+// Each UTS ns owns (hostname, domain). Defaults: ('localhost', '(none)').
 // Commands:
-//   NEWNS  -> create new userns; print id (1,2,...)
-//   MAP <ns> <in_id> <host_id> <length>  -> append range; OK or 'ERR overlap'
-//   TRANSLATE <ns> <in_id>  -> print host uid or 'unmapped'
-//   WHO <ns> <host_id>  -> print in-ns uid or 'unmapped'
+//   NEWNS  -> new UTS ns; print id
+//   SETHOST <ns> <name>  -> set hostname (max 64); OK or 'ERR too long'
+//   SETDOMAIN <ns> <name>  -> set domain; OK
+//   HOSTNAME <ns>  -> print hostname
+//   DOMAIN <ns>  -> print domain or '(none)'
+//   UNAME <ns>  -> print '<hostname> <domain>'
 
-type UidRange struct {
-	InStart, HostStart, Length int
-}
-
-func isOverlap(lower int, lowerLen int, upper int, upperLen int) bool {
-	return upper < lower+lowerLen || lower > upper+upperLen
+type UTS struct {
+	Host, Domain string
 }
 
 func main() {
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Buffer(make([]byte, 1<<20), 1<<24)
 	var out []string
+	var namespaces []UTS
 	// TODO: declare your state structures here
-	var namespaces [][]UidRange
 	for sc.Scan() {
 		line := strings.TrimSpace(sc.Text())
 		if line == "" {
@@ -38,78 +36,35 @@ func main() {
 		parts := strings.Fields(line)
 		switch parts[0] {
 		case "NEWNS":
-			// TODO: create new userns; print id (1,2,...)
-			namespaces = append(namespaces, []UidRange{})
+			// TODO: new UTS ns; print id
+			namespaces = append(namespaces, UTS{"localhost", "(none)"})
 			out = append(out, strconv.Itoa(len(namespaces)))
-		case "MAP":
-			// TODO: append range; OK or 'ERR overlap'
-			// For a new interval, overlaps exist if:
-			// new id <= existing id + length
-			// new id + length >= existing id
-			// This could happen for both in-ns *and* host ranges
+		case "SETHOST":
+			// TODO: set hostname (max 64); OK or 'ERR too long'
 			nsId, _ := strconv.Atoi(parts[1])
-			inId, _ := strconv.Atoi(parts[2])
-			hostId, _ := strconv.Atoi(parts[3])
-			length, _ := strconv.Atoi(parts[4])
-			newRange := UidRange{inId, hostId, length}
-			overlapFound := false
-			for _, r := range namespaces[nsId-1] {
-				var lo, loLen, hi, hiLen int
-				if r.InStart < newRange.InStart {
-					lo = r.InStart
-					loLen = r.Length
-					hi = newRange.InStart
-					hiLen = newRange.Length
-				} else {
-					lo = newRange.InStart
-					loLen = newRange.Length
-					hi = r.InStart
-					hiLen = r.Length
-				}
-				overlapFound = overlapFound || isOverlap(lo, loLen, hi, hiLen)
-				if r.HostStart < newRange.HostStart {
-					lo = r.HostStart
-					loLen = r.Length
-					hi = newRange.HostStart
-					hiLen = newRange.Length
-				} else {
-					lo = newRange.HostStart
-					loLen = newRange.Length
-					hi = r.HostStart
-					hiLen = r.Length
-				}
-				overlapFound = overlapFound || isOverlap(lo, loLen, hi, hiLen)
-			}
-			if overlapFound {
-				out = append(out, "ERR overlap")
+			if len(parts[2]) > 64 {
+				out = append(out, "ERR too long")
 			} else {
-				namespaces[nsId-1] = append(namespaces[nsId-1], newRange)
+				namespaces[nsId-1].Host = parts[2]
 				out = append(out, "OK")
 			}
-		case "TRANSLATE":
-			// TODO: print host uid or 'unmapped'
+		case "SETDOMAIN":
+			// TODO: set domain; OK
 			nsId, _ := strconv.Atoi(parts[1])
-			inId, _ := strconv.Atoi(parts[2])
-			res := "unmapped"
-			for _, r := range namespaces[nsId-1] {
-				if inId >= r.InStart && inId < r.InStart+r.Length {
-					res = strconv.Itoa(r.HostStart + (inId - r.InStart))
-					break
-				}
-			}
-			out = append(out, res)
-		case "WHO":
-			// TODO: print in-ns uid or 'unmapped'
+			namespaces[nsId-1].Domain = parts[2]
+			out = append(out, "OK")
+		case "HOSTNAME":
+			// TODO: print hostname
 			nsId, _ := strconv.Atoi(parts[1])
-			hostId, _ := strconv.Atoi(parts[2])
-			res := "unmapped"
-			for _, r := range namespaces[nsId-1] {
-				if hostId >= r.HostStart && hostId < r.HostStart+r.Length {
-					res = strconv.Itoa(hostId - r.HostStart)
-					break
-				}
-			}
-			out = append(out, res)
+			out = append(out, namespaces[nsId-1].Host)
+		case "DOMAIN":
+			// TODO: print domain or '(none)'
+			nsId, _ := strconv.Atoi(parts[1])
+			out = append(out, namespaces[nsId-1].Domain)
+		case "UNAME":
+			// TODO: print '<hostname> <domain>'
+			nsId, _ := strconv.Atoi(parts[1])
+			out = append(out, fmt.Sprintf("%s %s", namespaces[nsId-1].Host, namespaces[nsId-1].Domain))
 		}
 	}
 	fmt.Println(strings.Join(out, "\n"))
